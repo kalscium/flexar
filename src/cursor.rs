658bcf26fc618b67dde2c4218da56ec03a54489a
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-09-19 14:29:12 +1000

    fixed cursor

--- src/cursor.rs
+++ src/cursor.rs
@@ -1,10 +1,10 @@
-use std::rc::Rc;
+use std::{ops::Range, rc::Rc};
 
 /// Tracks the position of a character in a file
 #[derive(Clone)]
 pub struct Position {
     pub file_name: String,
-    pub file_contents: Rc<FileContents>,
+    file_contents: Rc<FileContents>,
     pub abs_idx: u16,
     pub ln: u16,
     pub ln_idx: u16,
@@ -37,24 +37,30 @@ impl Position {
     //     position.ln += 1;
     //     position.ln_idx = 1;
     // }
-}
 
-pub struct FileContents(pub Box<[Box<[char]>]>);
-
-impl FileContents {
-    pub fn new(contents: String) -> Self {
-        Self(contents.split('\n')
-            .map(|x| x.chars().collect())
-            .collect()
-        )
+    pub fn get_to<'a>(&'a self, other: &'a Self) -> Option<(&'a str, Range<usize>)> {
+        // if reversed, reverse it again to not cause any errors
+        if self.abs_idx > other.abs_idx {
+            return other.get_to(self);
+        }
+        
+        // This only works for one line, change it if needed
+        self.get_ln().map(|x| (x, (self.ln_idx as usize -1)..(other.ln_idx as usize -1))) // `ln_idx` starts at idx 1 instead of zero
     }
 
-    pub fn get(&self, position: &Position) -> Option<char> {
-        let line = match self.0.get(position.ln as usize -1) { // line starts at one instead of zero
-            Some(x) => x,
-            None => return None,
-        };
+    #[inline]
+    pub fn get_ln<'a>(&'a self) -> Option<&'a str> {
+        self.file_contents.0
+            .get(self.ln as usize -1) // line starts at one instead of zero
+            .map(|x| x.as_ref())
+    }
+}
 
-        line.get(position.ln_idx as usize -1).copied() // line idx starts at one instead of zero
+pub struct FileContents(pub Box<[Box<str>]>);
+
+impl FileContents {
+    #[inline]
+    pub fn new(contents: String) -> Self {
+        Self(contents.split('\n').map(|x| x.into()).collect())
     }
 }
