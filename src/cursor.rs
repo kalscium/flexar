a8c0c34673404839705e879386a817a18898c094
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-09-23 17:45:23 +1000

    added mutable cursor

--- src/cursor.rs
+++ src/cursor.rs
@@ -1,5 +1,53 @@
 use std::rc::Rc;
 
+/// A mutable cursor for parsing / lexing with
+#[derive(Debug, Clone)]
+pub struct MutCursor {
+    pub pos_start: Rc<Cursor>,
+    pub pos_end: Rc<Cursor>,
+    pub current_char: Option<char>,
+}
+
+impl MutCursor {
+    /// Constructs a new mutable cursor
+    pub fn new(pos_start: Cursor) -> Self {
+        let pos_start = Rc::new(pos_start);
+        Self {
+            pos_start: pos_start.clone(),
+            pos_end: pos_start,
+            current_char: None,
+        }
+    }
+
+    /// Constructs an immutable position from the ,`MutCursor`
+    #[inline]
+    pub fn position(&self) -> Position {
+        Position(self.pos_start.clone(), self.pos_end.clone())
+    }
+
+    /// Spawns a child `MutCursor`
+    pub fn spawn(&self) -> Self {
+        Self {
+            pos_start: self.pos_end.clone(),
+            pos_end: self.pos_end.clone(),
+            current_char: self.current_char,
+        }
+    }
+
+    /// Updates the last position of the `MutCursor`
+    #[inline]
+    pub fn update(&mut self, cursor: Cursor) {
+        self.pos_end = Rc::new(cursor);
+    }
+
+    /// Advances through the file
+    pub fn advance(&mut self) {
+        let mut cursor = self.pos_end.dupe();
+        self.current_char = cursor.advance();
+        self.update(cursor);
+    }
+}
+
 /// A full position of a string of characters in a file
 #[derive(Debug, Clone)]
 pub struct Position(pub Rc<Cursor>, pub Rc<Cursor>);
@@ -8,13 +56,6 @@ impl From<Cursor> for Position {
     fn from(cursor: Cursor) -> Self {
         let cursor = Rc::new(cursor);
         Self(cursor.clone(), cursor)
-    }
-}
-
-impl Position {
-    /// Spawns a child of the position; so that the child starts at the last cursor of this position
-    pub fn spawn(&self) -> Self {
-        Self(self.1.clone(), self.1.clone())
     }
 }
 
@@ -44,6 +85,35 @@ impl Cursor {
         self.file_contents.0
             .get(self.ln as usize -1) // line starts at one instead of zero
             .map(|x| x.as_ref())
+    }
+
+    /// Clones it through a Rc
+    #[inline]
+    pub fn dupe(&self) -> Self {
+        self.clone()
+    }
+
+    #[inline]
+    pub fn get_char(&self) -> Option<char> {
+        self.get_ln().unwrap()
+            .chars()
+            .enumerate()
+            .find(|(i, _)| *i == self.ln_idx as usize -1)
+            .map(|(_, x)| x)
+    }
+
+    pub fn advance(&mut self) -> Option<char> {
+        // for errors
+        self.ln_idx += 1; 
+        self.abs_idx += 1;
+
+        if self.ln_idx as usize > self.get_ln().unwrap().len() { // if reached end of line
+            if self.ln as usize == self.file_contents.0.len() { return None; } // if reached last line
+            self.ln += 1;
+            self.ln_idx = 1;
+        }
+
+        self.get_char()
     }
 }
 
